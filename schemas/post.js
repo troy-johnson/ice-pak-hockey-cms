fbb66b8b78fc88e3ff759cb516f0de04d78ee69f
troy.johnson <troy.johnson57@gmail.com>
2021-10-27 17:01:25 -0600

    updated post field descriptions

--- schemas/post.js
+++ schemas/post.js
@@ -7,21 +7,24 @@ export default {
       name: 'title',
       title: 'Title',
       type: 'string',
+      validation: Rule => Rule.required()
     },
     {
       name: 'slug',
-      title: 'Slug (click Generate)',
+      title: 'Slug (e.g. "recap-ice-pak-vs-flyers-10-24-21")',
       type: 'slug',
       options: {
         source: 'title',
         maxLength: 96,
       },
+      validation: Rule => Rule.required()
     },
     {
       name: 'author',
       title: 'Author',
       type: 'reference',
       to: {type: 'author'},
+      validation: Rule => Rule.required()
     },
     {
       name: 'mainImage',
@@ -30,6 +33,7 @@ export default {
       options: {
         hotspot: true,
       },
+      validation: Rule => Rule.required()
     },
     {
       name: 'categories',
@@ -39,13 +43,15 @@ export default {
     },
     {
       name: 'publishedAt',
-      title: 'Published at (leave blank for current time)',
+      title: 'Published at',
       type: 'datetime',
+      validation: Rule => Rule.required()
     },
     {
       name: 'body',
       title: 'Body',
       type: 'blockContent',
+      validation: Rule => Rule.required()
     },
   ],
 
